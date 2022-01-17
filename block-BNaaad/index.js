989619e44276ebe81a5dd71b0a6686cc830156c7
Abhijeet Mane <abhi@Varshas-MacBook-Air.local>
2022-01-17 20:13:54 +0530

    added Introduction to Node Js Assignment

---
+++ block-BNaaad/index.js
@@ -0,0 +1,14 @@
+console.log("Welcome to Node JS")
+
+function sum(a,b){
+    return a+b
+}
+console.log(sum(100,200));
+
+
+let val = 0;
+for(let i = 0; i < 10; i++){
+    console.log(i);
+    val += i
+}
+console.log(val)
